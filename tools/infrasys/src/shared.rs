sam-berning/bottlerocket-kernel-kit

infrasys: create TUF infra in AWS using `cargo make create-infra`
Adds binary (and supporting files) to automatically spin up TUF infra (S3 Bucket+Policy, KMS Keys, and populated root.json) across multiple regions in a single account.
Edits pubsys-config (and supporting files) to accomodate new infrasys fields in Infra.toml.

tools/infrasys/src/shared.rs
@@ -1,0 +1,91 @@
+use log::info;
+use rusoto_cloudformation::{CloudFormation, CloudFormationClient, DescribeStacksInput, Parameter};
+use snafu::{ensure, OptionExt, ResultExt};
+use std::{env, thread, time};
+use structopt::StructOpt;
+
+use super::{error, Result};
+
+#[derive(Debug, StructOpt)]
+pub enum KeyRole {
+    Root,
+    Publication,
+}
+
+/// Retrieve a BUILDSYS_* variable that we expect to be set in the environment
+pub fn getenv(var: &str) -> Result<String> {
+    env::var(var).context(error::Environment { var })
+}
+
+/// Generates a parameter type object used to specify parameters in CloudFormation templates
+pub fn create_parameter(key: String, val: String) -> Parameter {
+    Parameter {
+        parameter_key: Some(key),
+        parameter_value: Some(val),
+        ..Default::default()
+    }
+}
+
+/// Polls cfn_client for stack_name in region until it's ready
+/// Once stack is created, we can grab the outputs (before this point, outputs are empty)
+pub async fn get_stack_outputs(
+    cfn_client: &CloudFormationClient,
+    stack_name: &str,
+    region: &str,
+) -> Result<Vec<rusoto_cloudformation::Output>> {
+    let mut stack_outputs = cfn_client
+        .describe_stacks(DescribeStacksInput {
+            stack_name: Some(stack_name.to_string()),
+            ..Default::default()
+        })
+        .await
+        .context(error::DescribeStack { stack_name, region })?
+        .stacks
+        .context(error::ParseResponse {
+            what: "stacks",
+            resource_name: stack_name,
+        })?[0]
+        .clone();
+
+    // Checking that keys have been created so we can return updated outputs
+    let mut status = stack_outputs.stack_status;
+    // Max wait is 30 mins (90 attempts * 20s = 1800s = 30mins)
+    let mut max_attempts: u32 = 90;
+    while status != "CREATE_COMPLETE" {
+        ensure!(
+            max_attempts > 0,
+            error::CreateStackTimeout { stack_name, region }
+        );
+        ensure!(
+            status != "CREATE_FAILED",
+            error::CreateStackFailure { stack_name, region }
+        );
+        info!(
+            "Waiting for stack resources to be ready, current status is '{}'...",
+            status
+        );
+        thread::sleep(time::Duration::from_secs(20));
+        stack_outputs = cfn_client
+            .describe_stacks(DescribeStacksInput {
+                stack_name: Some(stack_name.to_string()),
+                ..Default::default()
+            })
+            .await
+            .context(error::DescribeStack { stack_name, region })?
+            .stacks
+            .context(error::ParseResponse {
+                what: "stacks",
+                resource_name: stack_name,
+            })?[0]
+            .clone();
+        status = stack_outputs.stack_status;
+        max_attempts -= 1;
+    }
+
+    let output_array = stack_outputs.outputs.context(error::ParseResponse {
+        what: "outputs",
+        resource_name: stack_name,
+    })?;
+
+    Ok(output_array)
+}

tools/pubsys-config/src/lib.rs
@@ -4,16 +4,17 @@ pub mod vmware;
 use crate::vmware::VmwareConfig;
 use chrono::Duration;
 use parse_datetime::parse_offset;
-use serde::{Deserialize, Deserializer};
+use serde::{Deserialize, Deserializer, Serialize};
 use snafu::ResultExt;
 use std::collections::{HashMap, VecDeque};
 use std::convert::TryFrom;
 use std::fs;
+use std::num::NonZeroUsize;
 use std::path::{Path, PathBuf};
 use url::Url;
 
 /// Configuration needed to load and create repos
-#[derive(Debug, Default, Deserialize)]
+#[derive(Debug, Default, Deserialize, Serialize, PartialEq)]
 #[serde(deny_unknown_fields)]
 pub struct InfraConfig {
     // Repo subcommand config
@@ -49,10 +50,31 @@ impl InfraConfig {
             Ok(Self::default())
         }
     }
+
+    /// Deserializes an InfraConfig from a Infra.lock file at a given path
+    pub fn from_lock_path<P>(path: P) -> Result<Self>
+    where
+        P: AsRef<Path>,
+    {
+        let path = path.as_ref();
+        let infra_config_str = fs::read_to_string(path).context(error::File { path })?;
+        serde_yaml::from_str(&infra_config_str).context(error::InvalidLock { path })
+    }
+}
+
+/// S3-specific TUF infrastructure configuration
+#[derive(Debug, Default, Deserialize, Serialize, PartialEq)]
+pub struct S3Config {
+    pub region: Option<String>,
+    #[serde(default)]
+    pub s3_prefix: String,
+    pub vpc_endpoint_id: Option<String>,
+    pub stack_arn: Option<String>,
+    pub bucket_name: Option<String>,
 }
 
 /// AWS-specific infrastructure configuration
-#[derive(Debug, Default, Deserialize)]
+#[derive(Debug, Default, Deserialize, Serialize, PartialEq)]
 #[serde(deny_unknown_fields)]
 pub struct AwsConfig {
     #[serde(default)]
@@ -62,10 +84,11 @@ pub struct AwsConfig {
     #[serde(default)]
     pub region: HashMap<String, AwsRegionConfig>,
     pub ssm_prefix: Option<String>,
+    pub s3: Option<HashMap<String, S3Config>>,
 }
 
 /// AWS region-specific configuration
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
 #[serde(deny_unknown_fields)]
 pub struct AwsRegionConfig {
     pub role: Option<String>,
@@ -76,12 +99,33 @@ pub struct AwsRegionConfig {
 // These variant names are lowercase because they have to match the text in Infra.toml, and it's
 // more common for TOML config to be lowercase.
 #[allow(non_camel_case_types)]
-#[derive(Clone, Debug, Deserialize)]
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
 #[serde(deny_unknown_fields)]
 pub enum SigningKeyConfig {
-    file { path: PathBuf },
-    kms { key_id: String },
-    ssm { parameter: String },
+    file {
+        path: PathBuf,
+    },
+    kms {
+        key_id: Option<String>,
+        #[serde(flatten)]
+        config: Option<KMSKeyConfig>,
+    },
+    ssm {
+        parameter: String,
+    },
+}
+
+/// AWS region-specific configuration
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
+//#[serde(deny_unknown_fields)]
+pub struct KMSKeyConfig {
+    #[serde(default)]
+    pub available_keys: HashMap<String, String>,
+    pub key_alias: Option<String>,
+    #[serde(default)]
+    pub regions: VecDeque<String>,
+    #[serde(default)]
+    pub key_stack_arns: HashMap<String, String>,
 }
 
 impl TryFrom<SigningKeyConfig> for Url {
@@ -91,8 +135,9 @@ impl TryFrom<SigningKeyConfig> for Url {
             SigningKeyConfig::file { path } => Url::from_file_path(path),
             // We don't support passing profiles to tough in the name of the key/parameter, so for
             // KMS and SSM we prepend a slash if there isn't one present.
-            SigningKeyConfig::kms { key_id } => {
-                let key_id = if key_id.starts_with('/') {
+            SigningKeyConfig::kms { key_id, .. } => {
+                let mut key_id = key_id.unwrap_or_else(Default::default);
+                key_id = if key_id.starts_with('/') {
                     key_id.to_string()
                 } else {
                     format!("/{}", key_id)
@@ -112,16 +157,18 @@ impl TryFrom<SigningKeyConfig> for Url {
 }
 
 /// Represents a Bottlerocket repo's location and the metadata needed to update the repo
-#[derive(Debug, Default, Deserialize)]
+#[derive(Debug, Default, Deserialize, Serialize, PartialEq)]
 #[serde(deny_unknown_fields)]
 pub struct RepoConfig {
     pub root_role_url: Option<Url>,
     pub root_role_sha512: Option<String>,
-
     pub signing_keys: Option<SigningKeyConfig>,
-
+    pub root_keys: Option<SigningKeyConfig>,
     pub metadata_base_url: Option<Url>,
     pub targets_url: Option<Url>,
+    pub file_hosting_config_name: Option<String>,
+    pub root_key_threshold: Option<NonZeroUsize>,
+    pub pub_key_threshold: Option<NonZeroUsize>,
 }
 
 /// How long it takes for each metadata type to expire
@@ -172,6 +219,12 @@ mod error {
         InvalidToml {
             path: PathBuf,
             source: toml::de::Error,
+        },
+
+        #[snafu(display("Invalid lock file at '{}': {}", path.display(), source))]
+        InvalidLock {
+            path: PathBuf,
+            source: serde_yaml::Error,
         },
 
         #[snafu(display("Missing config: {}", what))]

aashnasheth/bottlerocket

tools/infrasys/src/shared.rs
@@ -1,0 +1,91 @@
+use log::info;
+use rusoto_cloudformation::{CloudFormation, CloudFormationClient, DescribeStacksInput, Parameter};
+use snafu::{ensure, OptionExt, ResultExt};
+use std::{env, thread, time};
+use structopt::StructOpt;
+
+use super::{error, Result};
+
+#[derive(Debug, StructOpt)]
+pub enum KeyRole {
+    Root,
+    Publication,
+}
+
+/// Retrieve a BUILDSYS_* variable that we expect to be set in the environment
+pub fn getenv(var: &str) -> Result<String> {
+    env::var(var).context(error::Environment { var })
+}
+
+/// Generates a parameter type object used to specify parameters in CloudFormation templates
+pub fn get_parameter(key: String, val: String) -> Parameter {
+    Parameter {
+        parameter_key: Some(key),
+        parameter_value: Some(val),
+        ..Default::default()
+    }
+}
+
+/// Polls cfn_client for stack_name in region until it's ready
+/// Once stack is created, we can grab the outputs (before this point, outputs are empty)
+pub async fn get_stack_outputs(
+    cfn_client: &CloudFormationClient,
+    stack_name: &str,
+    region: &str,
+) -> Result<Vec<rusoto_cloudformation::Output>> {
+    let mut stack_outputs = cfn_client
+        .describe_stacks(DescribeStacksInput {
+            stack_name: Some(stack_name.to_string()),
+            ..Default::default()
+        })
+        .await
+        .context(error::DescribeStack { stack_name, region })?
+        .stacks
+        .context(error::ParseResponse {
+            what: "stacks",
+            resource_name: stack_name,
+        })?[0]
+        .clone();
+
+    // Checking that keys have been created so we can return updated outputs
+    let mut status = stack_outputs.stack_status;
+    // Max wait is 30 mins (90s*20s = 1800s = 30mins)
+    let mut max_attempts: u32 = 90;
+    while status != "CREATE_COMPLETE" {
+        ensure!(
+            max_attempts > 0,
+            error::CreateStackTimeout { stack_name, region }
+        );
+        ensure!(
+            status != "CREATE_FAILED",
+            error::CreateStackStatus { stack_name, region }
+        );
+        info!(
+            "Waiting for stack resources to be ready, current status is '{}'...",
+            status
+        );
+        thread::sleep(time::Duration::from_secs(20));
+        stack_outputs = cfn_client
+            .describe_stacks(DescribeStacksInput {
+                stack_name: Some(stack_name.to_string()),
+                ..Default::default()
+            })
+            .await
+            .context(error::DescribeStack { stack_name, region })?
+            .stacks
+            .context(error::ParseResponse {
+                what: "stacks",
+                resource_name: stack_name,
+            })?[0]
+            .clone();
+        status = stack_outputs.stack_status;
+        max_attempts -= 1;
+    }
+
+    let output_array = stack_outputs.outputs.context(error::ParseResponse {
+        what: "outputs",
+        resource_name: stack_name,
+    })?;
+
+    Ok(output_array)
+}

tools/pubsys-config/src/lib.rs
@@ -4,7 +4,7 @@ pub mod vmware;
 use crate::vmware::VmwareConfig;
 use chrono::Duration;
 use parse_datetime::parse_offset;
-use serde::{Deserialize, Deserializer};
+use serde::{Deserialize, Deserializer, Serialize};
 use snafu::ResultExt;
 use std::collections::{HashMap, VecDeque};
 use std::convert::TryFrom;
@@ -13,7 +13,7 @@ use std::path::{Path, PathBuf};
 use url::Url;
 
 /// Configuration needed to load and create repos
-#[derive(Debug, Default, Deserialize)]
+#[derive(Debug, Default, Deserialize, Serialize, PartialEq)]
 #[serde(deny_unknown_fields)]
 pub struct InfraConfig {
     // Repo subcommand config
@@ -49,10 +49,31 @@ impl InfraConfig {
             Ok(Self::default())
         }
     }
+
+    /// Deserializes an InfraConfig from a Infra.lock file at a given path
+    pub fn from_lock_path<P>(path: P) -> Result<Self>
+    where
+        P: AsRef<Path>,
+    {
+        let path = path.as_ref();
+        let infra_config_str = fs::read_to_string(path).context(error::File { path })?;
+        serde_yaml::from_str(&infra_config_str).context(error::InvalidLock { path })
+    }
+}
+
+/// S3-specific TUF infrastructure configuration
+#[derive(Debug, Default, Deserialize, Serialize, PartialEq)]
+pub struct S3Config {
+    pub region: Option<String>,
+    #[serde(default)]
+    pub s3_prefix: String,
+    pub vpc_endpoint_id: Option<String>,
+    pub stack_arn: Option<String>,
+    pub bucket_name: Option<String>,
 }
 
 /// AWS-specific infrastructure configuration
-#[derive(Debug, Default, Deserialize)]
+#[derive(Debug, Default, Deserialize, Serialize, PartialEq)]
 #[serde(deny_unknown_fields)]
 pub struct AwsConfig {
     #[serde(default)]
@@ -62,10 +83,11 @@ pub struct AwsConfig {
     #[serde(default)]
     pub region: HashMap<String, AwsRegionConfig>,
     pub ssm_prefix: Option<String>,
+    pub s3: Option<HashMap<String, S3Config>>,
 }
 
 /// AWS region-specific configuration
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Serialize, PartialEq)]
 #[serde(deny_unknown_fields)]
 pub struct AwsRegionConfig {
     pub role: Option<String>,
@@ -76,12 +98,33 @@ pub struct AwsRegionConfig {
 // These variant names are lowercase because they have to match the text in Infra.toml, and it's
 // more common for TOML config to be lowercase.
 #[allow(non_camel_case_types)]
-#[derive(Clone, Debug, Deserialize)]
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
 #[serde(deny_unknown_fields)]
 pub enum SigningKeyConfig {
-    file { path: PathBuf },
-    kms { key_id: String },
-    ssm { parameter: String },
+    file {
+        path: PathBuf,
+    },
+    kms {
+        key_id: Option<String>,
+        #[serde(flatten)]
+        config: Option<KMSKeyConfig>,
+    },
+    ssm {
+        parameter: String,
+    },
+}
+
+/// AWS region-specific configuration
+#[derive(Clone, Debug, Deserialize, Serialize, PartialEq)]
+//#[serde(deny_unknown_fields)]
+pub struct KMSKeyConfig {
+    #[serde(default)]
+    pub available_keys: HashMap<String, String>,
+    pub key_alias: Option<String>,
+    #[serde(default)]
+    pub regions: VecDeque<String>,
+    #[serde(default)]
+    pub key_stack_arns: HashMap<String, String>,
 }
 
 impl TryFrom<SigningKeyConfig> for Url {
@@ -91,8 +134,9 @@ impl TryFrom<SigningKeyConfig> for Url {
             SigningKeyConfig::file { path } => Url::from_file_path(path),
             // We don't support passing profiles to tough in the name of the key/parameter, so for
             // KMS and SSM we prepend a slash if there isn't one present.
-            SigningKeyConfig::kms { key_id } => {
-                let key_id = if key_id.starts_with('/') {
+            SigningKeyConfig::kms { key_id, .. } => {
+                let mut key_id = key_id.unwrap_or_else(Default::default);
+                key_id = if key_id.starts_with('/') {
                     key_id.to_string()
                 } else {
                     format!("/{}", key_id)
@@ -112,16 +156,18 @@ impl TryFrom<SigningKeyConfig> for Url {
 }
 
 /// Represents a Bottlerocket repo's location and the metadata needed to update the repo
-#[derive(Debug, Default, Deserialize)]
+#[derive(Debug, Default, Deserialize, Serialize, PartialEq)]
 #[serde(deny_unknown_fields)]
 pub struct RepoConfig {
     pub root_role_url: Option<Url>,
     pub root_role_sha512: Option<String>,
-
     pub signing_keys: Option<SigningKeyConfig>,
-
+    pub root_keys: Option<SigningKeyConfig>,
     pub metadata_base_url: Option<Url>,
     pub targets_url: Option<Url>,
+    pub file_hosting_config_name: Option<String>,
+    pub root_key_threshold: Option<String>,
+    pub pub_key_threshold: Option<String>,
 }
 
 /// How long it takes for each metadata type to expire
@@ -172,6 +218,12 @@ mod error {
         InvalidToml {
             path: PathBuf,
             source: toml::de::Error,
+        },
+
+        #[snafu(display("Invalid lock file at '{}': {}", path.display(), source))]
+        InvalidLock {
+            path: PathBuf,
+            source: serde_yaml::Error,
         },
 
         #[snafu(display("Missing config: {}", what))]