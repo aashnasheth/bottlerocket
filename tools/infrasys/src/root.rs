sam-berning/bottlerocket-kernel-kit

infrasys: create TUF infra in AWS using `cargo make create-infra`
Adds binary (and supporting files) to automatically spin up TUF infra (S3 Bucket+Policy, KMS Keys, and populated root.json) across multiple regions in a single account.
Edits pubsys-config (and supporting files) to accomodate new infrasys fields in Infra.toml.

tools/infrasys/src/root.rs
@@ -1,0 +1,193 @@
+use super::{error, KeyRole, Result};
+use log::{trace, warn};
+use pubsys_config::SigningKeyConfig;
+use rusoto_core::Region;
+use snafu::{ensure, OptionExt, ResultExt};
+use std::collections::HashMap;
+use std::fs;
+use std::num::NonZeroUsize;
+use std::path::Path;
+use std::process::Command;
+
+/// The tuftool macro wraps Command to simplify calls to tuftool, adding region functionality.
+macro_rules! tuftool {
+    ($region:expr, $format_str:expr, $($format_arg:expr),*) => {
+        let arg_str = format!($format_str, $($format_arg),*);
+        trace!("tuftool arg string: {}", arg_str);
+        let args = shell_words::split(&arg_str).context(error::CommandSplit { command: &arg_str })?;
+        trace!("tuftool split args: {:#?}", args);
+
+        let status = Command::new("tuftool")
+            .args(args)
+            .env("AWS_REGION", $region)
+            .status()
+            .context(error::TuftoolSpawn)?;
+
+        ensure!(status.success(), error::TuftoolResult {
+            command: arg_str,
+            code: status.code().map(|i| i.to_string()).unwrap_or_else(|| "<unknown>".to_string())
+        });
+    }
+}
+
+pub fn check_root(root_role_path: &Path) -> Result<()> {
+    ensure!(!root_role_path.is_file(), {
+        warn!("Cowardly refusing to overwrite the existing root.json at {}. Please manually delete it and run again.", root_role_path.display());
+        error::FileExists {
+            path: root_role_path,
+        }
+    });
+    Ok(())
+}
+
+/// Creates the directory where root.json will live and creates root.json itself according to details specified in root-role-path
+pub fn create_root(root_role_path: &Path) -> Result<()> {
+    // Make /roles and /keys directories, if they don't exist, so we can write generated files.
+    let role_dir = root_role_path.parent().context(error::InvalidPath {
+        path: root_role_path,
+        thing: "root role",
+    })?;
+    fs::create_dir_all(role_dir).context(error::Mkdir { path: role_dir })?;
+    // Initialize root
+    tuftool!(
+        Region::default().name(),
+        "root init '{}'",
+        root_role_path.display()
+    );
+    tuftool!(
+        Region::default().name(),
+        // TODO: expose expiration date as a configurable parameter
+        "root expire '{}' 'in 52 weeks'",
+        root_role_path.display()
+    );
+    Ok(())
+}
+
+/// Adds keys to root.json according to key type  
+pub fn add_keys(
+    signing_key_config: &mut SigningKeyConfig,
+    role: &KeyRole,
+    threshold: &NonZeroUsize,
+    filepath: &str,
+) -> Result<()> {
+    match signing_key_config {
+        SigningKeyConfig::file { .. } => (),
+        SigningKeyConfig::kms { key_id, config, .. } => add_keys_kms(
+            &config
+                .as_ref()
+                .context(error::MissingConfig {
+                    missing: "config field for a kms key",
+                })?
+                .available_keys,
+            role,
+            threshold,
+            filepath,
+            key_id,
+        )?,
+        SigningKeyConfig::ssm { .. } => (),
+    }
+    Ok(())
+}
+
+/// Adds KMSKeys to root.json given root or publication type
+/// Input: available-keys (keys to sign with), role (root or publication), threshold for role, filepath for root.JSON,
+/// mutable key_id
+/// Output: in-place edit of root.json and key_id with a valid publication key
+/// (If key-id is populated, it will not change. Otherwise, it will be populated with a key-id of an available key)
+fn add_keys_kms(
+    available_keys: &HashMap<String, String>,
+    role: &KeyRole,
+    threshold: &NonZeroUsize,
+    filepath: &str,
+    key_id: &mut Option<String>,
+) -> Result<()> {
+    ensure!(
+        (*available_keys).len() >= (*threshold).get(),
+        error::InvalidThreshold {
+            threshold: threshold.to_string(),
+            num_keys: (*available_keys).len(),
+        }
+    );
+
+    match role {
+        KeyRole::Root => {
+            tuftool!(
+                Region::default().name(),
+                "root set-threshold '{}' root '{}' ",
+                filepath,
+                threshold.to_string()
+            );
+            for (keyid, region) in available_keys.iter() {
+                tuftool!(
+                    region,
+                    "root add-key '{}' aws-kms:///'{}' --role root",
+                    filepath,
+                    keyid
+                );
+            }
+        }
+        KeyRole::Publication => {
+            tuftool!(
+                Region::default().name(),
+                "root set-threshold '{}' snapshot '{}' ",
+                filepath,
+                threshold.to_string()
+            );
+            tuftool!(
+                Region::default().name(),
+                "root set-threshold '{}' targets '{}' ",
+                filepath,
+                threshold.to_string()
+            );
+            tuftool!(
+                Region::default().name(),
+                "root set-threshold '{}' timestamp '{}' ",
+                filepath,
+                threshold.to_string()
+            );
+            for (keyid, region) in available_keys.iter() {
+                tuftool!(
+                region,
+                "root add-key '{}' aws-kms:///'{}' --role snapshot --role targets --role timestamp",
+                filepath,
+                keyid
+                );
+            }
+
+            // Set key_id using a publication key (if one is not already provided)
+            if key_id.is_none() {
+                *key_id = Some(
+                    available_keys
+                        .iter()
+                        .next()
+                        .context(error::KeyCreation)?
+                        .0
+                        .to_string(),
+                );
+            }
+        }
+    }
+
+    Ok(())
+}
+
+/// Signs root with available_keys under root_keys (will have a different tuftool command depending on key type)
+pub fn sign_root(signing_key_config: &SigningKeyConfig, filepath: &str) -> Result<()> {
+    match signing_key_config {
+        SigningKeyConfig::file { .. } => (),
+        SigningKeyConfig::kms { config, .. } => {
+            for (keyid, region) in config
+                .as_ref()
+                .context(error::MissingConfig {
+                    missing: "KMS key details",
+                })?
+                .available_keys
+                .iter()
+            {
+                tuftool!(region, "root sign '{}' -k aws-kms:///'{}'", filepath, keyid);
+            }
+        }
+        SigningKeyConfig::ssm { .. } => (),
+    }
+    Ok(())
+}

tools/infrasys/src/shared.rs
@@ -1,0 +1,91 @@
+use log::info;
+use rusoto_cloudformation::{CloudFormation, CloudFormationClient, DescribeStacksInput, Parameter};
+use snafu::{ensure, OptionExt, ResultExt};
+use std::{env, thread, time};
+use structopt::StructOpt;
+
+use super::{error, Result};
+
+#[derive(Debug, StructOpt)]
+pub enum KeyRole {
+    Root,
+    Publication,
+}
+
+/// Retrieve a BUILDSYS_* variable that we expect to be set in the environment
+pub fn getenv(var: &str) -> Result<String> {
+    env::var(var).context(error::Environment { var })
+}
+
+/// Generates a parameter type object used to specify parameters in CloudFormation templates
+pub fn create_parameter(key: String, val: String) -> Parameter {
+    Parameter {
+        parameter_key: Some(key),
+        parameter_value: Some(val),
+        ..Default::default()
+    }
+}
+
+/// Polls cfn_client for stack_name in region until it's ready
+/// Once stack is created, we can grab the outputs (before this point, outputs are empty)
+pub async fn get_stack_outputs(
+    cfn_client: &CloudFormationClient,
+    stack_name: &str,
+    region: &str,
+) -> Result<Vec<rusoto_cloudformation::Output>> {
+    let mut stack_outputs = cfn_client
+        .describe_stacks(DescribeStacksInput {
+            stack_name: Some(stack_name.to_string()),
+            ..Default::default()
+        })
+        .await
+        .context(error::DescribeStack { stack_name, region })?
+        .stacks
+        .context(error::ParseResponse {
+            what: "stacks",
+            resource_name: stack_name,
+        })?[0]
+        .clone();
+
+    // Checking that keys have been created so we can return updated outputs
+    let mut status = stack_outputs.stack_status;
+    // Max wait is 30 mins (90 attempts * 20s = 1800s = 30mins)
+    let mut max_attempts: u32 = 90;
+    while status != "CREATE_COMPLETE" {
+        ensure!(
+            max_attempts > 0,
+            error::CreateStackTimeout { stack_name, region }
+        );
+        ensure!(
+            status != "CREATE_FAILED",
+            error::CreateStackFailure { stack_name, region }
+        );
+        info!(
+            "Waiting for stack resources to be ready, current status is '{}'...",
+            status
+        );
+        thread::sleep(time::Duration::from_secs(20));
+        stack_outputs = cfn_client
+            .describe_stacks(DescribeStacksInput {
+                stack_name: Some(stack_name.to_string()),
+                ..Default::default()
+            })
+            .await
+            .context(error::DescribeStack { stack_name, region })?
+            .stacks
+            .context(error::ParseResponse {
+                what: "stacks",
+                resource_name: stack_name,
+            })?[0]
+            .clone();
+        status = stack_outputs.stack_status;
+        max_attempts -= 1;
+    }
+
+    let output_array = stack_outputs.outputs.context(error::ParseResponse {
+        what: "outputs",
+        resource_name: stack_name,
+    })?;
+
+    Ok(output_array)
+}

aashnasheth/bottlerocket

tools/infrasys/src/root.rs
@@ -1,0 +1,195 @@
+use super::{error, KeyRole, Result};
+use log::{trace, warn};
+use pubsys_config::SigningKeyConfig;
+use rusoto_core::Region;
+use snafu::{ensure, OptionExt, ResultExt};
+use std::collections::HashMap;
+use std::fs;
+use std::path::Path;
+use std::process::Command;
+
+/// The tuftool macro wraps Command to simplify calls to tuftool, adding region functionality.
+macro_rules! tuftool {
+    ($region:expr, $format_str:expr, $($format_arg:expr),*) => {
+        let arg_str = format!($format_str, $($format_arg),*);
+        trace!("tuftool arg string: {}", arg_str);
+        let args = shell_words::split(&arg_str).context(error::CommandSplit { command: &arg_str })?;
+        trace!("tuftool split args: {:#?}", args);
+
+        let status = Command::new("tuftool")
+            .args(args)
+            .env("AWS_REGION", $region)
+            .status()
+            .context(error::TuftoolSpawn)?;
+
+        ensure!(status.success(), error::TuftoolResult {
+            command: arg_str,
+            code: status.code().map(|i| i.to_string()).unwrap_or_else(|| "<unknown>".to_string())
+        });
+    }
+}
+
+pub fn check_root(root_role_path: &Path) -> Result<()> {
+    ensure!(!root_role_path.is_file(), {
+        warn!("Cowardly refusing to overwrite the existing root.json at {}. Please manually delete it and run again.", root_role_path.display());
+        error::FileExists {
+            path: root_role_path,
+        }
+    });
+    Ok(())
+}
+
+/// Creates the directory where root.json will live and creates root.json itself according to details specified in root-role-path
+pub fn create_root(root_role_path: &Path) -> Result<()> {
+    // Make /roles and /keys directories, if they don't exist, so we can write generated files.
+    let role_dir = root_role_path.parent().context(error::InvalidPath {
+        path: root_role_path,
+        thing: "root role",
+    })?;
+    fs::create_dir_all(role_dir).context(error::Mkdir { path: role_dir })?;
+    // Initialize root
+    tuftool!(
+        Region::default().name(),
+        "root init '{}'",
+        root_role_path.display()
+    );
+    tuftool!(
+        Region::default().name(),
+        // TODO: expose expiration date as a configurable parameter
+        "root expire '{}' 'in 52 weeks'",
+        root_role_path.display()
+    );
+    Ok(())
+}
+
+/// Adds keys to root.json according to key type  
+pub fn add_keys(
+    signing_key_config: &mut SigningKeyConfig,
+    role: &KeyRole,
+    threshold: &str,
+    filepath: &str,
+) -> Result<()> {
+    match signing_key_config {
+        SigningKeyConfig::file { .. } => (),
+        SigningKeyConfig::kms { key_id, config, .. } => add_keys_kms(
+            &config
+                .as_ref()
+                .context(error::MissingConfig {
+                    missing: "config field for a kms key",
+                })?
+                .available_keys,
+            role,
+            threshold,
+            filepath,
+            key_id,
+        )?,
+        SigningKeyConfig::ssm { .. } => (),
+    }
+    Ok(())
+}
+
+/// Adds KMSKeys to root.json given root or publication type
+/// Input: available-keys (keys to sign with), role (root or publication), threshold for role, filepath for root.JSON,
+/// mutable key_id
+/// Output: in-place edit of root.json and key_id with a valid publication key
+/// (If key-id is populated, it will not change. Otherwise, it will be populated with a key-id of an available key)
+fn add_keys_kms(
+    available_keys: &HashMap<String, String>,
+    role: &KeyRole,
+    threshold: &str,
+    filepath: &str,
+    key_id: &mut Option<String>,
+) -> Result<()> {
+    ensure!(
+        (*available_keys).len()
+            >= (*threshold)
+                .parse::<usize>()
+                .context(error::ParseInt { what: threshold })?,
+        error::InvalidThreshold {
+            threshold,
+            num_keys: (*available_keys).len(),
+        }
+    );
+
+    match role {
+        KeyRole::Root => {
+            tuftool!(
+                Region::default().name(),
+                "root set-threshold '{}' root '{}' ",
+                filepath,
+                threshold
+            );
+            for (keyid, region) in available_keys.iter() {
+                tuftool!(
+                    region,
+                    "root add-key '{}' aws-kms:///'{}' --role root",
+                    filepath,
+                    keyid
+                );
+            }
+        }
+        KeyRole::Publication => {
+            tuftool!(
+                Region::default().name(),
+                "root set-threshold '{}' snapshot '{}' ",
+                filepath,
+                threshold
+            );
+            tuftool!(
+                Region::default().name(),
+                "root set-threshold '{}' targets '{}' ",
+                filepath,
+                threshold
+            );
+            tuftool!(
+                Region::default().name(),
+                "root set-threshold '{}' timestamp '{}' ",
+                filepath,
+                threshold
+            );
+            for (keyid, region) in available_keys.iter() {
+                tuftool!(
+                region,
+                "root add-key '{}' aws-kms:///'{}' --role snapshot --role targets --role timestamp",
+                filepath,
+                keyid
+                );
+            }
+
+            // Set key_id using a publication key (if one is not already provided)
+            if key_id.is_none() {
+                *key_id = Some(
+                    available_keys
+                        .iter()
+                        .next()
+                        .context(error::KeyCreation)?
+                        .0
+                        .to_string(),
+                );
+            }
+        }
+    }
+
+    Ok(())
+}
+
+/// Signs root with available_keys under root_keys (will have a different tuftool command depending on key type)
+pub fn sign_root(signing_key_config: &SigningKeyConfig, filepath: &str) -> Result<()> {
+    match signing_key_config {
+        SigningKeyConfig::file { .. } => (),
+        SigningKeyConfig::kms { config, .. } => {
+            for (keyid, region) in config
+                .as_ref()
+                .context(error::MissingConfig {
+                    missing: "KMS key details",
+                })?
+                .available_keys
+                .iter()
+            {
+                tuftool!(region, "root sign '{}' -k aws-kms:///'{}'", filepath, keyid);
+            }
+        }
+        SigningKeyConfig::ssm { .. } => (),
+    }
+    Ok(())
+}

tools/infrasys/src/shared.rs
@@ -1,0 +1,91 @@
+use log::info;
+use rusoto_cloudformation::{CloudFormation, CloudFormationClient, DescribeStacksInput, Parameter};
+use snafu::{ensure, OptionExt, ResultExt};
+use std::{env, thread, time};
+use structopt::StructOpt;
+
+use super::{error, Result};
+
+#[derive(Debug, StructOpt)]
+pub enum KeyRole {
+    Root,
+    Publication,
+}
+
+/// Retrieve a BUILDSYS_* variable that we expect to be set in the environment
+pub fn getenv(var: &str) -> Result<String> {
+    env::var(var).context(error::Environment { var })
+}
+
+/// Generates a parameter type object used to specify parameters in CloudFormation templates
+pub fn get_parameter(key: String, val: String) -> Parameter {
+    Parameter {
+        parameter_key: Some(key),
+        parameter_value: Some(val),
+        ..Default::default()
+    }
+}
+
+/// Polls cfn_client for stack_name in region until it's ready
+/// Once stack is created, we can grab the outputs (before this point, outputs are empty)
+pub async fn get_stack_outputs(
+    cfn_client: &CloudFormationClient,
+    stack_name: &str,
+    region: &str,
+) -> Result<Vec<rusoto_cloudformation::Output>> {
+    let mut stack_outputs = cfn_client
+        .describe_stacks(DescribeStacksInput {
+            stack_name: Some(stack_name.to_string()),
+            ..Default::default()
+        })
+        .await
+        .context(error::DescribeStack { stack_name, region })?
+        .stacks
+        .context(error::ParseResponse {
+            what: "stacks",
+            resource_name: stack_name,
+        })?[0]
+        .clone();
+
+    // Checking that keys have been created so we can return updated outputs
+    let mut status = stack_outputs.stack_status;
+    // Max wait is 30 mins (90s*20s = 1800s = 30mins)
+    let mut max_attempts: u32 = 90;
+    while status != "CREATE_COMPLETE" {
+        ensure!(
+            max_attempts > 0,
+            error::CreateStackTimeout { stack_name, region }
+        );
+        ensure!(
+            status != "CREATE_FAILED",
+            error::CreateStackFailure { stack_name, region }
+        );
+        info!(
+            "Waiting for stack resources to be ready, current status is '{}'...",
+            status
+        );
+        thread::sleep(time::Duration::from_secs(20));
+        stack_outputs = cfn_client
+            .describe_stacks(DescribeStacksInput {
+                stack_name: Some(stack_name.to_string()),
+                ..Default::default()
+            })
+            .await
+            .context(error::DescribeStack { stack_name, region })?
+            .stacks
+            .context(error::ParseResponse {
+                what: "stacks",
+                resource_name: stack_name,
+            })?[0]
+            .clone();
+        status = stack_outputs.stack_status;
+        max_attempts -= 1;
+    }
+
+    let output_array = stack_outputs.outputs.context(error::ParseResponse {
+        what: "outputs",
+        resource_name: stack_name,
+    })?;
+
+    Ok(output_array)
+}